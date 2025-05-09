1c5dccfd259d5cd0821b78835b7f361afcb3c810
Dmitry Rzhenev <rzhenev.116@gmail.com>
2025-05-09 17:17:34 +0300

    4ЛР (15 Работа)

--- sitemobile/mobile_store/views.py
+++ sitemobile/mobile_store/views.py
@@ -1,38 +1,18 @@
 from itertools import product
 from django.shortcuts import render
 
-data = { "company": "ООО Мобильная связь"}
+data = { "company": "ООО Илья молодец"}
 
 def index(request):
+    data["active_page"] = "index"            
     return render(request, "mobile_store/index.html", context=data)
 
 
 def dashboard(request):
+    data["active_page"] = "dashboard"
     return render(request, "mobile_store/dashboard.html", context=data)
 
 
 def inventory(request):
-    products = [{
-            "id": 1, 
-            "name": "iPhone 14",
-            "category": "Телефоны",
-            "quantity": 10,
-            "price": 100000,
-            "status_text": "В наличии"
-    }, {
-            "id": 2,
-            "name": "iPhone 13",
-            "category": "Телефоны",
-            "quantity": 0,
-            "price": 80000,
-            "status_text": "Нет в наличии"
-    }, {
-            "id": 3,
-            "name": "iPhone 12",
-            "category": "Телефоны",
-            "quantity": 5,
-            "price": 60000,
-            "status_text": "В наличии"
-    }]
-    data["products"] = products
+    data["active_page"] = "inventory"
     return render(request, "mobile_store/inventory.html", context=data)
